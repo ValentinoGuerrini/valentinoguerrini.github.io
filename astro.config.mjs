042a0475445584bd2968c8300bbcd74633d084c2
Valentino Guerrini <vale@Valentinos-MacBook-Pro.local>
2025-10-01 17:44:48 -0500

    adjust

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  
+  vite: {
+    plugins: [tailwindcss()],
+  },
   site: 'https://valentinoguerrini.github.io',
   // no `base` needed for a personal site repo named <username>.github.io
 });
